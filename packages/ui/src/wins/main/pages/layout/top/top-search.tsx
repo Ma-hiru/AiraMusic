4f6c9f7bf39f8be7ff06cc64b1ee2d3111a57ce2
Ma-hiru <tenshimahiru@qq.com>
2026-08-02 01:38:52 +0800

    docs: 更新文档

--- packages/ui/src/wins/main/pages/layout/top/top-search.tsx
+++ packages/ui/src/wins/main/pages/layout/top/top-search.tsx
@@ -21,7 +21,7 @@ const TopSearch: FC<object> = () => {
       <div
         className="h-5 rounded-full border px-2 py-1 text-[10px] font-semibold flex justify-center items-center select-none border-white/10 bg-white/10 shadow-sm cursor-pointer"
         onClick={() => openSearch(defaultKeywords ?? undefined)}>
-        <span>{defaultKeywords}</span>
+        <span>{defaultKeywords || "搜索"}</span>
       </div>
       <Search
         className="size-5 cursor-pointer hover:opacity-50 ease-in-out transition-all duration-300 active:scale-90"
